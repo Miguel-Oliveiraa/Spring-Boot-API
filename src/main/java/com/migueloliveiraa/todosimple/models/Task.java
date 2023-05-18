3946261e2e03223613562b4088ee1cf494b8f7da
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-05-18 17:53:31 -0300

    feat: Create Task model

---
+++ src/main/java/com/migueloliveiraa/todosimple/models/Task.java
@@ -0,0 +1,74 @@
+package com.migueloliveiraa.todosimple.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@Table(name = Task.TABLE_NAME)
+public class Task {
+    public static final String TABLE_NAME = "task";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @Column(name = "description", length = 255, nullable = false)
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 255)
+    private String description;
+
+    public Task() {
+    }
+
+    public Task(Long id, User user, String description) {
+        this.id = id;
+        this.user = user;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Task task = (Task) obj;
+        return Objects.equals(id, task.id) && Objects.equals(user, task.user) && Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, description);
+    }
+}
