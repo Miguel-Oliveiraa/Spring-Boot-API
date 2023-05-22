23ea07051b04286e8c2bf922522c1963dff3e3b0
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-05-22 20:06:59 -0300

    refactor: Models Data using Lombok

--- src/main/java/com/migueloliveiraa/todosimple/models/Task.java
+++ src/main/java/com/migueloliveiraa/todosimple/models/Task.java
@@ -1,13 +1,19 @@
 package com.migueloliveiraa.todosimple.models;
 
+import lombok.*;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Objects;
 
 @Entity
 @Table(name = Task.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Task {
     public static final String TABLE_NAME = "task";
 
@@ -25,50 +31,4 @@ public class Task {
     @NotEmpty
     @Size(min = 1, max = 255)
     private String description;
-
-    public Task() {
-    }
-
-    public Task(Long id, User user, String description) {
-        this.id = id;
-        this.user = user;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Task task = (Task) obj;
-        return Objects.equals(id, task.id) && Objects.equals(user, task.user) && Objects.equals(description, task.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, user, description);
-    }
 }
