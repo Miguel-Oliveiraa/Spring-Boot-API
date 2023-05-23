d3e2cd8babd6be92e26fc5cd1a52c07ef7041043
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-05-23 20:35:14 -0300

    feat: Create FindByUsername

--- src/main/java/com/migueloliveiraa/todosimple/repositories/UserRepository.java
+++ src/main/java/com/migueloliveiraa/todosimple/repositories/UserRepository.java
@@ -3,7 +3,11 @@ package com.migueloliveiraa.todosimple.repositories;
 import com.migueloliveiraa.todosimple.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Transactional(readOnly = true)
+    User findByUsername(String username);
 }
