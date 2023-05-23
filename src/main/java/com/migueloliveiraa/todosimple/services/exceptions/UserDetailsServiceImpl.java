f9b7503fa2c44c481a0a6ee2836af73e6ebfa371
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-05-23 20:35:43 -0300

    feat: Create User Details dev temp class

---
+++ src/main/java/com/migueloliveiraa/todosimple/services/exceptions/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.migueloliveiraa.todosimple.services.exceptions;
+
+import com.migueloliveiraa.todosimple.models.User;
+import com.migueloliveiraa.todosimple.repositories.UserRepository;
+import com.migueloliveiraa.todosimple.security.UserSpringSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+        if (Objects.isNull(user))
+            throw new UsernameNotFoundException("Usuário não encontrado: " + username);
+        return new UserSpringSecurity(user.getId(), user.getUsername(), user.getPassword(), user.getProfiles());
+    }
+}
